4c88e88a77afc1b93a851fd12527a5ec42fcb2ca
Ninz11 <148561307+Nigestyh@users.noreply.github.com>
2025-10-24 17:01:32 +0100

    Add list content to Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,19 @@ function Main() {
   return (
     <main>
       <h1>Reasons I am excited to learn React</h1>
+      <ul>
+        <li>
+          React's custom components promote a highly composable approach to UI
+          development, making it easier to build complex interfaces
+        </li>
+        <li>
+          The component-based structure speeds up development cycle by enabling
+          reusability
+        </li>
+        <li>It is supported by a large ecosystem and community</li>
+        <li>Highest job demand</li>
+        <li>Powers thousand of enterprise apps, including mobile apps</li>
+      </ul>
     </main>
   );
 }
