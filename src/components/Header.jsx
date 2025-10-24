8c1e24c5062d421c3b85e5b9d76e525589da79d1
Ninz11 <148561307+Nigestyh@users.noreply.github.com>
2025-10-24 16:37:30 +0100

    Add components folder and two components: Header and Main

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <nav>
+      <span>React</span>
+    </nav>
+  );
+}
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+function Main() {
+  return (
+    <main>
+      <h1>Reasons I am excited to learn React</h1>
+    </main>
+  );
+}
+export default Main;
