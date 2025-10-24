eb17dd79f7f1f4530dfb4cfd9c7b9f8189e117e7
Ninz11 <148561307+Nigestyh@users.noreply.github.com>
2025-10-24 16:53:48 +0100

    Add img asset and style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 function Header() {
   return (
-    <nav>
-      <span>React</span>
-    </nav>
+    <header>
+      <nav>
+        <img src="src/assets/react.svg" alt="react-logo" />
+        <span>React</span>
+      </nav>
+    </header>
   );
 }
 export default Header;
